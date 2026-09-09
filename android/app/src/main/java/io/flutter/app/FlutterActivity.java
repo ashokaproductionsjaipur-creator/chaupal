50c4c23d79e3a9c0f004b3f78d60458d5e59d9da
ashokaproductionsjaipur-creator <ashokaproductionsjaipur@gmail.com>
2026-09-09 11:56:19 +0530

    chore(android): add v1 embedding compatibility marker

---
+++ android/app/src/main/java/io/flutter/app/FlutterActivity.java
@@ -0,0 +1,9 @@
+package io.flutter.app;
+
+/**
+ * Temporary compatibility marker for projects whose build tooling still scans
+ * for the removed Android v1 embedding package.
+ */
+@Deprecated
+public class FlutterActivity {
+}
